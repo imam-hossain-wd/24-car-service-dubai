f39c9c766d0024beaadc04342d4e99c24bd9a89e
imam-hossain-wd <imamhossain6t9@gmail.com>
2025-05-27 02:18:56 +0600

    24 car service dubai Porject setup complete

---
+++ src/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Best Car Repair and Services in Dubai || 24 Car Service Dubai",
+  description: "One of the best car repair and servicing company in dubai || 24 Car Service Dubai",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
